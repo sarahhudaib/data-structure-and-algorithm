0f83d728a78059f5a3dd2bd899f5ebee647f4ff0
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-02-28 15:49:51 +0300

    add class 01

---
+++ Challenges/class01/test.py
@@ -0,0 +1,23 @@
+list = ['A', 'B', 'C', 'D']
+
+def reverse(array):
+    return array[::-1]
+
+print(reverse(list))
+
+
+
+# pseudocode
+
+# This program reverse an array
+
+# send ann array with the name of list to the below function call
+# list = ['A', 'B', 'C', 'D']
+# go to the last item in the array
+# Then return it and in the same way do all the elements
+# Return the whole array reversed.
+
+# def reverse(array):
+#     return array[::-1]
+# print the array in reverse 
+# print(reverse(list))
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# data-structure-and-algorithm
+# Data Structure and Algorithm
+## Table of Contents
+| Code  | Title  | Link  |
+|:-:|:-:|:-:|
+| Class 01  | Reverse an array  | [Link](./Challenges/class01/README.md)  |
+|   |   |   |
+|   |   |   |
