53388a873b62a30c927685494c2f629c1b71a6ea
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-02 10:37:00 +0300

    add test

--- Challenges/class02/test.py
+++ Challenges/class02/test.py
@@ -14,3 +14,15 @@ def insert_shift_array(array , value):
     
 
 print(insert_shift_array(list,value))
+
+
+def test_insert_shift_even():
+  actual = insert_shift_array([1, 2, 4, 5], 3)
+  expected = [1, 2, 3, 4, 5]
+  assert actual == expected
+
+
+def test_insert_shift_odd():
+  actual = insert_shift_array([1, 2, 3, 5, 6], 4)
+  expected = [1, 2, 3, 4, 5, 6]
+  assert actual == expected
