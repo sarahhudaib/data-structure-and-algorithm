5ab7d840bbb0e82ccaf0f4bdbedbfcd8d9fcf21b
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-19 17:44:39 +0300

    adding Linked List kth

--- data_structuers/linked_list/linked_list.py
+++ data_structuers/linked_list/linked_list.py
@@ -100,9 +100,7 @@ the first node that has the value specified'''
     def kth_from_end(self, k):
         """kth from end
         argument: a number, k, as a parameter.
-        Return the node’s value that is k places from the tail of the linked list.
-        You have access to the Node class and all the properties on the Linked List class as well as 
-        the methods created in previous challenges."""
+        Return the nodes value that is k places from the tail of the linked list """
         
         length = self.length_()
         if not -length <= k < length:
